257b5a6b68034a69786207c5daafeb724e8aaa8b
khirvy019 <khirvy_19@yahoo.com>
2023-02-21 16:29:54 +0800

    update create child nft example

--- examples/slp/nft1/parent/create_child_nft.js
+++ examples/slp/nft1/parent/create_child_nft.js
@@ -2,12 +2,7 @@ const Watchtower = require('../../../../src')
 
 const watchtower = new Watchtower()
 
-const groupTokenBalData = {
-  groupTokenId: 'f019cfa73559836c13e00d70e7105d4d43377bb6a9861595a7b2373a66aa0bc7', // <-- NFT parent token ID
-  wallet: 'simpleledger:qpq82xgmau3acnuvypkyj0khks4a6ak7zqj6ffwnh8' // <-- address or wallet hash
-}
-
-const mintBatonData = {
+const createChildNftData = {
   sender: {
     address: 'simpleledger:qp3et5cla7jju6z2lfc5v9nr0r4q54edqqdl5mxfjc',
     wif: 'XXX'  // <-- private key of the sender address
@@ -18,23 +13,6 @@ const mintBatonData = {
   },
   groupTokenId: 'f019cfa73559836c13e00d70e7105d4d43377bb6a9861595a7b2373a66aa0bc7', // <-- NFT parent token ID
   recipient: 'simpleledger:qp3et5cla7jju6z2lfc5v9nr0r4q54edqqdl5mxfjc', // <-- only 1 since every NFT is unique and amount is always 1
-  // (Optional) <-- set a custom change BCH address (fee funder address by default)
-  changeAddress: 'bitcoincash:qp46gzcw0ycxtnngrhp0xddp2qxnyjnepg2qc02eeh',
-  // (Optional) <-- Broadcast or just return raw transaction hex
-  broadcast: true  // true by default
-}
-
-const createChildNftData = {
-  sender: {
-    address: 'simpleledger:qqz95enwd6qdcy5wnf05hp590sjjknwfuq8sjhpv6x',
-    wif: 'XXX'  // <-- private key of the sender address
-  },
-  feeFunder: {
-    address: 'bitcoincash:qq46tffgznfew8e78dkyt56k9xcmetnelcma256km7',
-    wif: 'YYY' // <-- private key of the feeFunder address
-  },
-  groupTokenId: 'f019cfa73559836c13e00d70e7105d4d43377bb6a9861595a7b2373a66aa0bc7', // <-- NFT parent token ID
-  recipient: 'simpleledger:qpq82xgmau3acnuvypkyj0khks4a6ak7zqj6ffwnh8', // <-- only 1 since every NFT is unique and amount is always 1
   label: 'My Unique NFT Token',
   ticker: 'UNI-NFT', // <-- NFT symbol / abbreviation
   docUrl: 'https://uninft.com', // (Optional) <-- Document URL of token
@@ -44,40 +22,42 @@ const createChildNftData = {
   broadcast: true  // true by default
 }
 
+const findMintingBatonData = {
+  groupTokenId: createChildNftData.groupTokenId,
+  address: createChildNftData.sender.address
+}
 
-// check Parent Group token balance
-watchtower.SLP.NFT1.Parent.getGroupTokenBalance(groupTokenBalData).then(result => {
-  if (result.success) {
-    console.log(result.balance)
-  } else {
-    console.log(result.error)
-  }
-})
+const mintBatonData = {
+  sender: {
+    address: createChildNftData.sender.address,
+    wif: createChildNftData.sender.wif  // <-- private key of the sender address
+  },
+  feeFunder: {
+    address: createChildNftData.feeFunder.address,
+    wif: createChildNftData.feeFunder.wif // <-- private key of the feeFunder address
+  },
+  groupTokenId: createChildNftData.groupTokenId, // <-- NFT parent token ID
+  recipient: createChildNftData.sender.address, // <-- only 1 since every NFT is unique and amount is always 1
+  // (Optional) <-- set a custom change BCH address (fee funder address by default)
+  changeAddress: createChildNftData.changeAddress,
+  // (Optional) <-- Broadcast or just return raw transaction hex
+  broadcast: true  // true by default
+}
 
-watchtower.SLP.NFT1.Parent.generateMintingBatonUtxo(mintBatonData).then(result => {
-  console.log('MINT baton UTXO result:')
-  if (result.success) {
-    // Your logic here when send transaction is successful
-    console.log(result.txid)
 
-    // or if broadcast is set to false, you can just get the raw transaction hex
-    console.log(result.transaction)
-    
-    watchtower.SLP.NFT1.Parent.createChildNft(createChildNftData).then(result => {
-      console.log('Create Child NFT result')
+watchtower.SLP.NFT1.Parent.findChildNftMintingBaton(findMintingBatonData)
+  .then(async (mintingUtxo) => {
+    if (!mintingUtxo) {
+      console.log('Creating minting baton')
+      const mintingBatonResult = await watchtower.SLP.NFT1.Parent.generateMintingBatonUtxo(mintBatonData)
+      console.log('Created minting baton:', mintingBatonResult)
+    } else {
+      console.log('Found minting baton:', mintingUtxo)
+    }
 
-      if (result.success) {
-        // Your logic here when send transaction is successful
-        console.log(result.txid)
-
-        // or if broadcast is set to false, you can just get the raw transaction hex
-        console.log(result.transaction)
-      } else {
-        // logic when it fails
-        console.log(result.error)
-      }
-    })
-  } else {
-    console.log(result.error)
-  }
-})
+    console.log('Creating child nft')
+    watchtower.SLP.NFT1.Parent.createChildNft(createChildNftData)
+      .then(console.log)
+      .catch(console.error)
+  })
+  .catch(console.error)
